2631677bb3fe82e2d2845ce91b0094782de30879
richrobber2 <richardrgguno@hotmail.com>
2024-11-30 19:33:58 -0800

    Refactor safe.py to enhance code readability by simplifying exception handling and using more specific exception types

--- modules/safe.py
+++ modules/safe.py
@@ -16,8 +16,7 @@ from modules import errors
 TypedStorage = torch.storage.TypedStorage if hasattr(torch.storage, 'TypedStorage') else torch.storage._TypedStorage
 
 def encode(*args):
-    out = _codecs.encode(*args)
-    return out
+    return _codecs.encode(*args)
 
 
 class RestrictedUnpickler(pickle.Unpickler):
@@ -61,7 +60,7 @@ class RestrictedUnpickler(pickle.Unpickler):
             return set
 
         # Forbid everything else.
-        raise Exception(f"global '{module}/{name}' is forbidden")
+        raise ImportError(f"global '{module}/{name}' is forbidden")
 
 
 # Regular expression that accepts 'dirname/version', 'dirname/byteorder', 'dirname/data.pkl', '.data/serialization_id', and 'dirname/data/<number>'
@@ -73,7 +72,7 @@ def check_zip_filenames(filename, names):
         if allowed_zip_names_re.match(name):
             continue
 
-        raise Exception(f"bad file inside {filename}: {name}")
+        raise ValueError(f"bad file inside {filename}: {name}")
 
 
 def check_pt(filename, extra_handler):
@@ -85,10 +84,10 @@ def check_pt(filename, extra_handler):
 
             # find filename of data.pkl in zip file: '<directory name>/data.pkl'
             data_pkl_filenames = [f for f in z.namelist() if data_pkl_re.match(f)]
-            if len(data_pkl_filenames) == 0:
-                raise Exception(f"data.pkl not found in {filename}")
+            if not data_pkl_filenames:
+                raise FileNotFoundError(f"data.pkl not found in {filename}")
             if len(data_pkl_filenames) > 1:
-                raise Exception(f"Multiple data.pkl found in {filename}")
+                raise ValueError(f"Multiple data.pkl found in {filename}")
             with z.open(data_pkl_filenames[0]) as file:
                 unpickler = RestrictedUnpickler(file)
                 unpickler.extra_handler = extra_handler
