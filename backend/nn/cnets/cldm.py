c11010fd2fccd104b5461c5aca841e25c1f2b8b9
richrobber2 <richardrgguno@hotmail.com>
2024-11-30 19:04:50 -0800

    Refactor cldm.py to improve error handling by rearranging conditions for num_res_blocks validation

--- backend/nn/cnets/cldm.py
+++ backend/nn/cnets/cldm.py
@@ -61,11 +61,11 @@ class ControlNet(nn.Module):
 
         if isinstance(num_res_blocks, int):
             self.num_res_blocks = len(channel_mult) * [num_res_blocks]
-        else:
-            if len(num_res_blocks) != len(channel_mult):
-                raise ValueError("provide num_res_blocks either as an int (globally constant) or as a list/tuple (per-level) with the same length as channel_mult")
+        elif len(num_res_blocks) == len(channel_mult):
             self.num_res_blocks = num_res_blocks
 
+        else:
+            raise ValueError("provide num_res_blocks either as an int (globally constant) or as a list/tuple (per-level) with the same length as channel_mult")
         if disable_self_attentions is not None:
             assert len(disable_self_attentions) == len(channel_mult)
         if num_attention_blocks is not None:
